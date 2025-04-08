e801f497b6ca22210e94fc2fb047a0c533f4cc36
gjpgabayeron <gjpgabayeron@gmail.com>
2025-04-08 10:25:43 +0800

    Add Logomark component to Navbar and adjust header styling for better layout

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
+import { Logomark } from '../icons'
 
 const Navbar = () => {
   return (
-    <header className="navbar bg-base-300 shadow-2xl">
-        <Link href="./" className="btn btn-ghost text-xl">evote</Link>
+    // Fit content hieght
+    <header className="navbar bg-base-300 shadow-2xl sticky top-0 z-50">
+        <Link href="./" className="btn btn-ghost text-xl"><Logomark/></Link>
     </header>
   )
 }
